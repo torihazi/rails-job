254e1777afd0a880c3aba9f43c2ca4248c3e27fb
torihazi <lovebarusa19981225@icloud.com>
2025-11-08 14:40:39 +0900

    user model job

--- jobs-basic/config/routes.rb
+++ jobs-basic/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "hello", to: "hello#hello"
   get "notify", to: "notification#notify"
   get "report", to: "report#report"
+  get "first_user", to: "user#first_user"
 end
